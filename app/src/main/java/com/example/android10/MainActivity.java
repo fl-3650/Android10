cc90de240829f6452eeb9f5c6334f24c0fbb915b
fl_3650 <repilovi@gmail.com>
2024-05-14 16:54:03 +0300

    1 done

--- app/src/main/java/com/example/android10/MainActivity.java
+++ app/src/main/java/com/example/android10/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android10;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("username", "User123");
+        editor.putInt("sessionCount", 5);
+        editor.putBoolean("loggedIn", true);
+        editor.apply();
+
+        sharedPreferences =
+                getSharedPreferences("myPreferences", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "defaultUsername");
+        int sessionCount = sharedPreferences.getInt("sessionCount", 0);
+        boolean isLoggedIn = sharedPreferences.getBoolean("loggedIn", false);
+
+        editor = sharedPreferences.edit();
+        editor.remove("username");
+        editor.clear();
+        editor.apply();
     }
 }
